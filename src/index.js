c73620c94518a0961f200437b1a4938b91e0ed99
Andrey <36437353+AndreyBaglai@users.noreply.github.com>
2021-02-25 22:54:23 +0200

    Task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array) {
+    if (!arguments.length || !array.length) return 0;
 
-exports.min = function min (array) {
-  return 0;
-}
+    return Math.min(...array);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!arguments.length || !array.length) return 0;
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return Math.max(...array);
+};
+
+exports.avg = function avg(array) {
+    if (!arguments.length || !array.length) return 0;
+
+    return array.reduce((sum, next) => sum += next) / array.length;
+};
